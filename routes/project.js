568f9ea6ce9a67bbb4045748f1d883955e8c58f5
Whitney LaRow <wlarow@stanford.edu>
2014-02-19 15:10:26 -0800

    putting it together lab

--- routes/project.js
+++ routes/project.js
@@ -3,8 +3,9 @@ var models = require('../models');
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
-  // query for the specific project and
-  // call the following callback
+  models.Project
+    .find({ '_id': projectID })
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -16,13 +17,28 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
-  // make a new Project and save it to the DB
-  // YOU MUST send an OK response w/ res.send();
+  form_data.title = form_data.project_title;
+
+  var newProj = new models.Project(form_data);
+  newProj.save(afterSaving);
+
+  function afterSaving(err) {
+    if(err) console.log(err);
+    res.send();
+  };
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
-  // find the project and remove it
-  // YOU MUST send an OK response w/ res.send();
+  console.log('deleting '+projectID);
+  models.Project
+    .find({ '_id': projectID })
+    .remove()
+    .exec(deleteCallback);
+
+  function deleteCallback(err) {
+    if(err) console.log(err);
+    res.send();
+  }
 }
